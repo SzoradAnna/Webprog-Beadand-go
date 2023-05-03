276d2b8e4272ea80b3b5a4e00d5e14920ef1c02e
Weller Dániel <weller.daniel@ckik.hu>
2023-05-03 12:19:56 +0200

    start gomb és design

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 const tablaMeret = 8; // 8x8-as sakktábla
 const tabla = document.getElementById('tabla');
+var gomb = false
 
 // Loop az oszlopok és sorok létrehozásához
-for (let sor = 0; sor < tablaMeret; sor++) {
-  for (let oszlop = 0; oszlop < tablaMeret; oszlop++) {
-    const cella = document.createElement('div');
-    tabla.appendChild(cella);
-
-    // Paritás ellenőrzése az oszlop és sor alapján
-    if ((sor + oszlop) % 2 === 0) {
-      cella.classList.add('paros');
-    } else {
-      cella.classList.add('paratlan');
+function Jatekgeneralas(){
+if(!gomb){
+  for (let sor = 0; sor < tablaMeret; sor++) {
+    for (let oszlop = 0; oszlop < tablaMeret; oszlop++) {
+      const cella = document.createElement('div');
+      tabla.appendChild(cella);
+      // Paritás ellenőrzése az oszlop és sor alapján
+      if ((sor + oszlop) % 2 === 0) {
+        cella.classList.add('paros');
+      } else {
+        cella.classList.add('paratlan');
+      }
+      tabla.classList.add('szele')
+      // Cella tartalmának beállítása (pl. koordináták, bábuk)
+      // Ezt a részt még ki kell egészíteni a konkrét sakktábla logikával
     }
-
-    // Cella tartalmának beállítása (pl. koordináták, bábuk)
-    // Ezt a részt még ki kell egészíteni a konkrét sakktábla logikával
-    
   }
+  gomb = true;
 }
+}
+
